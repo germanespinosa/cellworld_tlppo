715109f88ee0465dc34915504238498d6ec2922a
German <germanespinosa@gmail.com>
2024-09-11 13:27:44 -0700

    added world batch

--- tlppo_test.py
+++ tlppo_test.py
@@ -7,12 +7,15 @@ from test_conditions import get_tlppo, get_belief_state_components
 import os
 
 create_video = False
-result_folders = "TLPPO_unity"
+result_folders = "TLPPO_FINAL"
 os.makedirs(result_folders, exist_ok=True)
 episode_count = 100
 
-worlds = ["00_00", "00_03", "00_08", "01_00", "01_03", "01_08", "02_00", "02_03", "02_08", "03_00", "03_03", "03_08"]
-for world in worlds:
+worlds1 = ['00_01', '00_02', '00_03', '00_04', '00_05', '00_06', '00_07', '00_08', '00_09', '01_01', '01_02', '01_03', '01_04', '01_05', '01_06', '01_07', '01_08', '01_09', '02_01', '02_02', '02_03', '02_04', '02_05', '02_06', '02_07', '02_08', '02_09', '03_01', '03_02', '03_03', '03_04', '03_05', '03_06', '03_07', '03_08', '03_09', '04_01', '04_02', '04_03', '04_04', '04_05', '04_06', '04_07', '04_08', '04_09']
+worlds2 = ['05_01', '05_02', '05_03', '05_04', '05_05', '05_06', '05_07', '05_08', '05_09', '06_01', '06_02', '06_03', '06_04', '06_05', '06_06', '06_07', '06_08', '06_09', '07_01', '07_02', '07_03', '07_04', '07_05', '07_06', '07_07', '07_08', '07_09', '08_01', '08_02', '08_03', '08_04', '08_05', '08_06', '08_07', '08_08', '08_09', '09_01', '09_02', '09_03', '09_04', '09_05', '09_06', '09_07', '09_08', '09_09']
+worlds3 = ['10_01', '10_02', '10_03', '10_04', '10_05', '10_06', '10_07', '10_08', '10_09', '11_01', '11_02', '11_03', '11_04', '11_05', '11_06', '11_07', '11_08', '11_09', '12_01', '12_02', '12_03', '12_04', '12_05', '12_06', '12_07', '12_08', '12_09', '13_01', '13_02', '13_03', '13_04', '13_05', '13_06', '13_07', '13_08', '13_09', '14_01', '14_02', '14_03', '14_04', '14_05', '14_06', '14_07', '14_08', '14_09']
+
+for world in worlds1:
     for condition in [4]:
         for depth in [1, 5]:
             process = False
@@ -23,7 +26,7 @@ for world in worlds:
 
                 data_point_file = f"{data_point_experiment_name}.json"
                 data_point_file_path = f"{data_point_results_folder}/{data_point_file}"
-                print(f"Condition: {condition}, Depth: {depth}, Budget: {budget}", end="")
+                print(f"Condition: {condition}, World: {world}, Depth: {depth}, Budget: {budget}", end="")
                 if os.path.exists(data_point_file_path):
                     print("... already exists")
                     continue
@@ -62,7 +65,7 @@ for world in worlds:
                     obs, _ = environment.reset()
                     finished, truncated = False, False
                     puff_count = 0
-                    while not finished and not truncated:
+                    while not finished and not truncated and environment.model.prey_data.puff_count == 0:
                         action = tlppo.get_action(point=environment.model.prey.state.location,
                                                   discount=0.0,
                                                   exploration=math.inf)
